25aeb5a417025db2afdd7d04c12f59daaf56e5b7
GANGHWI LEE <rkdgnlee@gmail.com>
2026-06-18 13:50:33 +0900

    API 경로 난독

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // 💡 1. 관리자 API 로컬 프록시 설정
+        source: '/tbad-404/:path*',
+        destination: `https://${process.env.API_ADMIN_SERVER_URL}/:path*`,
+      },
+      {
+        // 💡 2. 파일/결과물 로컬 프록시 설정
+        source: '/k9-v2r/:path*',
+        destination: `https://${process.env.API_ADMIN_FILE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
